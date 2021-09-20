f239c51944769fe33a075f79f594234424e669a2
Nephael <67867993+Nephael@users.noreply.github.com>
2021-09-21 05:34:24 +0900

    Create 3. es6.js

---
+++ src/3. es6.js
@@ -0,0 +1,12 @@
+// es5 방식
+function Es5() { //es5는 function으로 선언한다.
+    console.log('hello world');
+}
+
+// es6 방식
+const Es6 = () => { //es6 방식은 const로 선언한다.
+    console.log('hello world');
+}
+
+Es5();
+Es6();
